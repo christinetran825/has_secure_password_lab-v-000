f8bae483ac04561db24bb63f735981ee6e29a1c9
Christine Tran <christinetran825@gmail.com>
2017-11-22 04:53:02 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
 
   def index
   end
-  
+
   def new
     @user = User.new
   end
@@ -11,9 +11,9 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       session[:user_id] = @user.id
-      redirect '/users/new'
+      redirect users_path
     else
-      redirect '/'
+      redirect new_user_path
     end
   end
 
