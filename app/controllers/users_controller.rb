2575470367b2d3be656235522353937e42afa826
Christine Tran <christinetran825@gmail.com>
2017-11-22 04:37:02 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,10 @@
 class UsersController < ApplicationController
 
   def create
+    user = User.new(user_params).save
+  end
 
+  def user_params
+    params.require(:user).permit(:username, :password, :password_confirmation)
   end
 end
